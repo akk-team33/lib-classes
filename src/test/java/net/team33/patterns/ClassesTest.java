30d0e691e7717e6a5115d582ac2bd0f533947e43
Andi Kluge <andi.kluge@mobi4friends.de>
2018-03-23 09:22:32 +0100

    Initial commit

---
+++ src/test/java/net/team33/patterns/ClassesTest.java
@@ -0,0 +1,12 @@
+package net.team33.patterns;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ClassesTest {
+
+    @Test
+    public final void any() {
+        Assert.assertTrue(false);
+    }
+}
